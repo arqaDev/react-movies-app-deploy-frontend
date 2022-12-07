b441e61fe251d8f35b6dce97a93bc24584b32729
arqaDev <arqa1996@gmail.com>
2022-12-07 12:12:01 +0300

    changed

--- src/http/userApi.ts
+++ src/http/userApi.ts
@@ -8,7 +8,6 @@ import { Context } from "../index";
 // create user and add token to localstorage
 export const registration = async (email: string, password: string): Promise<any> => {
     const { data } = await $host.post('api/user/registration', {email, password})
-    // localStorage.setItem('token', data.token)
     
     return jwt_decode(data.token)
 }
@@ -16,7 +15,6 @@ export const registration = async (email: string, password: string): Promise<any
 // make login and add token to localstorage
 export const login = async (email: string, password: string): Promise<any> => {
     const { data } = await $host.post('api/user/login', {email, password})
-    // localStorage.setItem('token', data.token)
 
     return jwt_decode(data.token)
 }
@@ -24,7 +22,6 @@ export const login = async (email: string, password: string): Promise<any> => {
 // get data and add token to localstorage
 export const check = async (): Promise<any> => {
     const { data } = await $authHost.get('api/user/auth')
-    // localStorage.setItem('token', data.token)
     
     return jwt_decode(data.token)
 }
--- src/store/MoviesStore.ts
+++ src/store/MoviesStore.ts
@@ -7,12 +7,10 @@ import { check } from "../http/userApi";
 export default class MoviesStore{
     _wishlistMovies: IMovieDetails[]
     _wishlistQuantity: number
-    _LS: number | never
 
     constructor() {
         this._wishlistMovies = []   
         this._wishlistQuantity = 0
-        this._LS = 0
         makeAutoObservable(this)
     }
 
@@ -24,25 +22,14 @@ export default class MoviesStore{
         this._wishlistQuantity = num
     }
 
-    setLS(id: number): void {
-        this._LS = id
-    }
-
     get wishlistQuantity(): number {
         return this._wishlistQuantity
     }
 
     get LS(): number | never {
-        const { id }:any = check()
-        // try {
-            // const jwt = localStorage.getItem('token')
-            // const decodedToken: any = jwt_decode(jwt!)
-            // const wishlistId = decodedToken.id
-            // return wishlistId
-            return id
-        // } catch (e) {
-        //     throw Error('you are not logged in')
-        // }    
+        const { id }: any = check()
+        
+        return id
     }
 
     get wishlistMovies(): IMovieDetails[] {
